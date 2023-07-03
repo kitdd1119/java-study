c5c28f272f04c15ea9f420bf6a7df9fbdb87b800
kitdd1119 <kitdd1119@gmail.com>
2023-07-03 17:03:14 +0900

    ch06추가

---
+++ ch06/S02.java
@@ -0,0 +1,42 @@
+// // dog앞에서
+// // 쉬프트 에프 6을 누르면 전체 코드를 바꿀 수 있음
+// class Dog {
+// String name;
+// int position;
+
+// public Dog(String name, int position) {
+// this.name = name;
+// this.position = position;
+// System.out.println(this.name + "가 태어났습니다.");
+// }
+
+// public Dog(String name) {
+// this(name, 0);
+// }
+
+// public void moveRight() {
+// this.position++;
+// System.out.println(this.name + "가 오른쪽으로 이동");
+// }
+
+// public void moveLeft() {
+// this.position--;
+// System.out.println(this.name + "가 왼쪽으로 이동");
+// }
+
+// }
+
+// public class S02 {
+// public static void main(String[] args) {
+// Dog dog = new Dog("멍멍이");
+
+// // 유저가 캐릭터를 움직임
+// dog.moveRight();
+// dog.moveLeft();
+
+// // 고양이 위치 확인
+// System.out.println(dog.position);
+
+// }
+
+// }
---
+++ ch06/S04.java
@@ -0,0 +1,27 @@
+// 실행 오류 확인해보기
+// class Cat {
+// static int countCat = 0;
+// String name;
+
+// public Cat() {
+// this.name = "길고양이";
+// Cat.addCat();
+// }
+
+// public static void addCat() {
+// Cat.countCat++;
+// System.out.println("새로운 고양이가 태어났습니다.");
+// }
+// }
+
+// public class S04 {
+// public static void main(String[] args) {
+// Cat cat1 = new Cat(); ?
+// new Cat();
+// new Cat();
+// new Cat();
+// new Cat();
+
+// System.out.println("고양이의 수는 " + Cat.countCat);
+// }
+// }
